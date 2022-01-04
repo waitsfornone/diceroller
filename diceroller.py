6bea36116c208e4953cf95f2763ccba86575074e
Tim Enders <enders.tim@gmail.com>
2022-01-04 17:31:44 -0600

    further shrinking of lines of code

--- diceroller.py
+++ diceroller.py
@@ -14,55 +14,44 @@ def roll(dice_type, check, save, char, skill, equipment):
         data = json.load(inf)
         # print(data)
     if check:
-        roll_val = dx(20)
         try:
-            mod_val = int(data[check])
-            click.echo(roll_val + mod_val)
+            click.echo(dx(20) + int(data[check]))
         except KeyError:
             click.echo(f"No {check} found on character")
     elif save:
-        roll_val = dx(20)
         try:
-            mod_val = int(data["save"][save])
-            click.echo(roll_val + mod_val)
+            click.echo(dx(20) + int(data["save"][save]))
         except KeyError:
             click.echo(f"No {save} found on character")
     elif skill:
-        roll_val = dx(20)
         try:
-            mod_val = int(data["skill"][skill])
-            click.echo(roll_val + mod_val)
+            click.echo(dx(20) + int(data["skill"][skill]))
         except KeyError:
             click.echo(f"No {skill} found on character")
     elif equipment:
         try:
             obj = data["equipment"][equipment]
             hit_roll = dx(20)
-            hit_mod = obj["hit"]
-            click.echo(f"Hit: {hit_roll + hit_mod}")
+            click.echo(f"Hit: {hit_roll + int(obj['hit'])}")
             dmg_roll = multi_roll(obj["dmg_dice"])
-            dmg_mod = obj["dmg_mod"]
             if hit_roll == 20:
                 click.echo("CRIT")
                 dmg_roll2 = multi_roll(obj["dmg_dice"])
-                click.echo(f"Damage: {sum(dmg_roll) + dmg_mod + sum(dmg_roll2)}")
+                click.echo(f"Damage: {sum(dmg_roll) + obj['dmg_mod'] + sum(dmg_roll2)}")
             else:
-                click.echo(f"Damage: {sum(dmg_roll) + dmg_mod}")
+                click.echo(f"Damage: {sum(dmg_roll) + obj['dmg_mod']}")
         except KeyError:
             click.echo(f"{equipment} not found in your equipment!")
     elif dice_type:
-        rolled_dice = multi_roll(dice_type)
-        click.echo(sum(rolled_dice))
+        click.echo(sum(multi_roll(dice_type)))
     else:
         click.echo("Initiative Roll!!!")
-        roll_val = dx(20)
         mod_att = data.get("class", None)
         if not mod_att:
             click.echo("Class attribute not set!")
         else:
             try:
-                mod_val = data[mod_att]
-                click.echo(roll_val + mod_val)
+                click.echo(dx(20) + data[mod_att])
             except KeyError:
                 click.echo(f"No {mod_att} found on character")
 
